5316f9b4fa31c4af888b5f4656f48fd7f13f607f
Sidh <sidharta.rezende@gmail.com>
2018-08-08 18:57:18 -0300

    Inicial
    Inicial

---
+++ TheTimer/src/main/java/br/com/gasinflamavel/TheTimer/App.java
@@ -0,0 +1,13 @@
+package br.com.gasinflamavel.TheTimer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
